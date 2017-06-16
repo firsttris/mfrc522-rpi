fe8d60bd7cab974cbe1de3421214e8bfb2b825c9
Tristan Teufel <tristanteufel@googlemail.com>
2017-06-16 17:08:55 +0200

    NTAG213 Test

---
+++ test/dumpNTAG213.js
@@ -0,0 +1,38 @@
+"use strict";
+const mfrc522 = new (require("./../index"))();
+
+
+//# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+console.log("scanning...");
+console.log("Please put chip or keycard in the antenna inductive zone!");
+console.log("Press Ctrl-C to stop.");
+
+setInterval(function(){
+
+    //# Scan for cards
+    let response = mfrc522.findCard();
+    if (!response.status) {
+        return;
+    }
+    console.log("Card detected, CardType: " + response.bitSize);
+
+    //# Get the UID of the card
+    response = mfrc522.getUid();
+    if (!response.status) {
+        console.log("UID Scan Error");
+        return;
+    }
+    //# If we have the UID, continue
+    const uid = response.data;
+    console.log("Card read UID: %s %s %s %s", uid[0].toString(16), uid[1].toString(16), uid[2].toString(16), uid[3].toString(16));
+
+    //# Select the scanned card
+    const memoryCapacity = mfrc522.selectCard(uid);
+    console.log("Card Memory Capacity: " + memoryCapacity);
+
+    //# dump fifo buffer
+    for (let i = 0; i < 45; i++) {
+            console.log("Block: "+i+" Data: "+mfrc522.getDataForBlock(i));
+    }
+
+}, 500);
